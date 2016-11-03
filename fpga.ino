1524a951ec8e3bea26342dee242a1a96611c0841
Isaías <isaiahlima18@gmail.com>
2016-11-03 19:58:16 -0200

    Primeiro teste

--- fpga.ino
+++ fpga.ino
@@ -1,16 +1,21 @@
+// sinais do FPGA
 int TX = 2;
 const byte clk = 3;
-int right = 4;
-int wrong = 5;
+
+// varáiveis auxiliares
+int jogadas = 0;
+int acertos = 0;
+int erros = 0;
+bool dobradinhas = 0;
+
 bool mustRead = false;
 
 void setup() {
   Serial.begin(9600);
   Serial.flush();
+  Serial.print("Bem-vindo ao jogo de Cara ou Coroa mais legal de todos os tempos");
   pinMode(TX, INPUT);
   pinMode(clk, INPUT_PULLUP);
-  pinMode(right, OUTPUT);
-  pinMode(wrong, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(clk), bordered, FALLING);
 }
 
@@ -19,24 +24,31 @@ void loop() {
     int tx = digitalRead(TX);
     if(tx == HIGH) {
       Serial.print("Acertooooooooou miserávi\n");
-      digitalWrite(wrong, HIGH);
-      digitalWrite(right, LOW);
-      delay(500);
-      digitalWrite(wrong, LOW);
+      acertos++;
+      Serial.print(acertos);
+      Serial.print(" acertos e ");
+      Serial.print(erros);
+      Serial.print(" erros\n");
       mustRead = false;
     } else {
-      Serial.print("Errroooooou\n");
-      digitalWrite(wrong, LOW);
-      digitalWrite(right, HIGH);
-      delay(500);
-      digitalWrite(right, LOW);
+      Serial.print("Errroooooooooou\n");
+      erros++;
+      Serial.print(acertos);
+      Serial.print(" acertos e ");
+      Serial.print(erros);
+      Serial.print(" erros\n");
       mustRead = false;
     }
   }
 }
 
 void bordered() {
-  mustRead = true;
-  Serial.print("CLOCKED\n");
+  if(dobradinhas) {
+    mustRead = true;
+    dobradinhas = !dobradinhas;
+  } else {
+    mustRead = false;
+    dobradinhas = !dobradinhas;
+  }
 }
 
